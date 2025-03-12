772f14349ccd0108fb059109712193f7460ce4f4
ishan <ishanvadalia1607@gmail.com>
2025-03-13 00:20:01 +0530

    pagination added

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
-import { Container, Typography, Card, CardContent, CircularProgress } from "@mui/material";
+import { Container, Typography, Card, CardContent, CircularProgress, Button } from "@mui/material";
 
 import RepoDetails from './RepoDetails';
 import CreateRepoForm from './CreateRepoForm';
 
 const RepoList = () => {
-  const { loading, error, data, refetch } = useQuery(GET_REPOSITORIES);
+  const { loading, error, data, fetchMore, refetch } = useQuery(GET_REPOSITORIES, {
+    variables: { first: 10 }, // Fetch only 5 repositories at a time
+  });
   const [selectedRepo, setSelectedRepo] = useState(null);
 
   if (loading) return <CircularProgress />;
@@ -16,6 +18,27 @@ const RepoList = () => {
   if (selectedRepo) {
     return <RepoDetails repo={selectedRepo} onBack={() => setSelectedRepo(null)} />;
   }
+
+  const repositories = data.viewer.repositories.nodes;
+  const { hasNextPage, endCursor } = data.viewer.repositories.pageInfo;
+
+  const loadMoreRepos = () => {
+    fetchMore({
+      variables: { first: 5, after: endCursor },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prevResult;
+        return {
+          viewer: {
+            ...prevResult.viewer,
+            repositories: {
+              ...fetchMoreResult.viewer.repositories,
+              nodes: [...prevResult.viewer.repositories.nodes, ...fetchMoreResult.viewer.repositories.nodes],
+            },
+          },
+        };
+      },
+    });
+  };
 
   return (
     <Container maxWidth="md" style={{ marginTop: '20px' }}>
@@ -27,7 +50,7 @@ const RepoList = () => {
       <CreateRepoForm onRepoCreated={refetch} style={{ marginBottom: '16px', padding: '10px', boxShadow: '2px 4px 10px rgba(0,0,0,0.1)', cursor: 'pointer' }} />
 
       {/* List of Repositories */}
-      {data.viewer.repositories.nodes.map((repo) => (
+      {repositories.map((repo) => (
         <Card
           key={repo.id}
           onClick={() => setSelectedRepo(repo)}
@@ -46,6 +69,19 @@ const RepoList = () => {
           </CardContent>
         </Card>
       ))}
+
+      {/* Pagination: Load More Button */}
+      {hasNextPage && (
+        <Button
+          onClick={loadMoreRepos}
+          variant="contained"
+          color="primary"
+          fullWidth
+          style={{ marginTop: '16px' }}
+        >
+          Load More
+        </Button>
+      )}
     </Container>
   );
 };
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,9 +1,9 @@
 import { gql } from '@apollo/client';
 
 export const GET_REPOSITORIES = gql`
-  query GetRepositories {
+  query GetRepositories($first: Int, $after: String) {
     viewer {
-      repositories(first: 10, orderBy: { field: UPDATED_AT, direction: DESC }) {
+      repositories(first: $first, after: $after, orderBy: { field: UPDATED_AT, direction: DESC }) {
         nodes {
           id
           name
@@ -13,6 +13,10 @@ export const GET_REPOSITORIES = gql`
           owner {
             login
           }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
         }
       }
     }
