80e817003f8a82812a9ae5bf583f77f9d4c4661f
ishan <ishanvadalia1607@gmail.com>
2025-03-12 23:33:40 +0530

    token updated

--- src/ApolloClient.jsx
+++ src/ApolloClient.jsx
@@ -1,12 +1,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const GITHUB_TOKEN = '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';
-
 const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://api.github.com/graphql',
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
     },
   }),
   cache: new InMemoryCache(),
